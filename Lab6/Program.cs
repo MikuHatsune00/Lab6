99e1b5ae719254958defabb130bd16ec38d84d58
Kseniia <balerinagizelle@mail.ru>
2021-11-23 11:25:26 +0300

    Add project files.

---
+++ Lab6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            char[] chars = { ' ' };
+            string[] stringArray = startString.Split(chars);
+            int max = stringArray[0].Length;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i].Length > max)
+                    max = stringArray[i].Length;
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
